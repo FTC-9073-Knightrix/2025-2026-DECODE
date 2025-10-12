9cc7506200ca4c80396c458f72f657498ebe255b
Mehxeo <utsavd500@gmail.com>
2025-10-12 13:49:45 -0400

    changed hoodouttaktest to have velocity instead of power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test/HoodOuttakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test/HoodOuttakeTest.java
@@ -5,20 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 public class HoodOuttakeTest extends OpMode {
     public Servo hoodServo;
-    public DcMotorEx outtakeMotor; //output velocity in ticks and angles into telemetry
+    public DcMotorEx outtakeMotor;
 
     public double leftPosition = 0.0;
     public double rightPosition = 1.0;
-
-    public double velocity;
-
-    public double motorPower = 1.0;
 
     private boolean outtakeOn = false;
     private boolean lastAState = false;
@@ -28,7 +23,7 @@ public class HoodOuttakeTest extends OpMode {
     private boolean lastDpadDown = false;
 
     static final double TICKS_PER_REV = 28;
-
+    public double targetVelocityTicks = 0.0;
 
     @Override
     public void init() {
@@ -37,63 +32,62 @@ public class HoodOuttakeTest extends OpMode {
 
         outtakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         outtakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        hoodServo.setPosition(leftPosition); // Set to a neutral position
+        hoodServo.setPosition(leftPosition);
     }
 
     @Override
     public void loop() {
-        // Toggle outtake motor on/off with gamepad1.a
+        // Toggle motor on/off
         if (gamepad1.a && !lastAState) {
             outtakeOn = !outtakeOn;
         }
         lastAState = gamepad1.a;
 
-        if (outtakeOn) {
-            outtakeMotor.setPower(motorPower);
-        } else {
-            outtakeMotor.setPower(0.0);
-        }
-
-        // Dpad left/right to adjust hoodServo position
+        // Servo adjust
         if (gamepad1.dpad_left && !lastDpadLeft) {
-            leftPosition -= 0.05;
-            if (leftPosition < 0.0) leftPosition = 0.0;
+            leftPosition = Math.max(0.0, leftPosition - 0.05);
             hoodServo.setPosition(leftPosition);
         }
         lastDpadLeft = gamepad1.dpad_left;
 
         if (gamepad1.dpad_right && !lastDpadRight) {
-            leftPosition += 0.05;
-            if (leftPosition > 1.0) leftPosition = 1.0;
+            leftPosition = Math.min(1.0, leftPosition + 0.05);
             hoodServo.setPosition(leftPosition);
         }
         lastDpadRight = gamepad1.dpad_right;
 
-        // Dpad up/down to adjust motorPower
+        // Adjust velocity
         if (gamepad1.dpad_up && !lastDpadUp) {
-            motorPower += 0.05;
-            if (motorPower > 1.0) motorPower = 1.0;
+            targetVelocityTicks += 100;
+            if (targetVelocityTicks > 2000) targetVelocityTicks = 2000;
         }
         lastDpadUp = gamepad1.dpad_up;
 
         if (gamepad1.dpad_down && !lastDpadDown) {
-            motorPower -= 0.05;
-            if (motorPower < 0.0) motorPower = 0.0;
+            targetVelocityTicks -= 100;
+            if (targetVelocityTicks < 0) targetVelocityTicks = 0;
         }
         lastDpadDown = gamepad1.dpad_down;
 
-        velocity = outtakeMotor.getVelocity();
-        double ticksPerSecond = outtakeMotor.getVelocity(); // already in ticks/sec
+        // Apply velocity control
+        if (outtakeOn) {
+            outtakeMotor.setVelocity(targetVelocityTicks);
+        } else {
+            outtakeMotor.setVelocity(0);
+        }
+
+        // Telemetry
+        double ticksPerSecond = outtakeMotor.getVelocity();
         double revPerSecond = ticksPerSecond / TICKS_PER_REV;
         double radPerSecond = revPerSecond * 2 * Math.PI;
 
         telemetry.addData("Outtake On", outtakeOn);
-        telemetry.addData("Motor Power", motorPower);
-        telemetry.addData("Servo Position", leftPosition);
-        telemetry.addData("Velocity (ticks/sec)", ticksPerSecond);
-        telemetry.addData("Velocity (ang)", outtakeMotor.getVelocity(AngleUnit.DEGREES));
+        telemetry.addData("Target Velocity (ticks/sec)", targetVelocityTicks);
+        telemetry.addData("Current Velocity (ticks/sec)", ticksPerSecond);
+        telemetry.addData("Velocity (deg/sec)", outtakeMotor.getVelocity(AngleUnit.DEGREES));
         telemetry.addData("Velocity (rev/sec)", revPerSecond);
         telemetry.addData("Angular Velocity (rad/sec)", radPerSecond);
+        telemetry.addData("Servo Position", leftPosition);
         telemetry.update();
     }
 }

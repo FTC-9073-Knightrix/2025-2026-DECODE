af135d05e23b7161151914a25e73966656c2f1ca
Mehxeo <utsavd500@gmail.com>
2025-11-15 01:55:17 -0500

    coded 12 ball auto positions with roadrunner

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,46 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.Vector2d;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-56, 45, Math.toRadians(37)))
+                .strafeToLinearHeading(new Vector2d(-20, 20), Math.toRadians(135))
+                // make it wait here
+                .strafeToLinearHeading(new Vector2d(-20, 30), Math.toRadians(90))// shooting 1st ball
+                //wait for 3 seconds
+                // .turn(Math.toRadians(60)) lets say u want to miss the ball
+                .strafeToLinearHeading(new Vector2d(-10, 50), Math.toRadians(60)) // goes for closest row and intakes it
+                .strafeToLinearHeading(new Vector2d(-20, 20), Math.toRadians(135)) // shooting ball 2nd time
+                //wait for 3 seconds
+                .strafeToLinearHeading(new Vector2d(14, 30), Math.toRadians(90)) // goes for middle row
+                .strafeToLinearHeading(new Vector2d(14, 50), Math.toRadians(90)) // intakes middle row
+                .strafeToLinearHeading(new Vector2d(-20, 20), Math.toRadians(135)) // shooting ball 3rd time
+                //wait for 3 seconds
+                // go for last row (farthest row)
+                .strafeToLinearHeading(new Vector2d(38, 30), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(38, 45), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(38, 50), Math.toRadians(90))  // intakes all of the last row
+                // return to shooting position
+                .strafeToLinearHeading(new Vector2d(-20, 20), Math.toRadians(135)) // shooting ball 4th time
+                // wait for 3 seconds
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
